409c2c6278f6100195cc3244bbd052d5a2e36e50
Alexandru Loghin <58110813+Aleku99@users.noreply.github.com>
2023-05-09 18:29:52 +0300

    feat: even more seo (#10)
    * feat: added links for facebook/instagram, added email in contact page and improved SEO
    
    Signed-off-by: Alexandru Loghin <loghinalex19@gmail.com>
    
    * feat: even more seo improvements
    
    Signed-off-by: Alexandru Loghin <loghinalex19@gmail.com>
    
    ---------
    
    Signed-off-by: Alexandru Loghin <loghinalex19@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,11 +8,6 @@ export const metadata = {
     "Luxury Detailing este o companie stabilita in Caransebes ce oferă servicii profesionale de detaliere auto pentru a restaura și menține frumusețea vehiculului dumneavoastră.",
   keywords: [
     "car detailing",
-    "auto detailing",
-    "paint correction",
-    "ceramic coating",
-    "engine detailing",
-    "headlight restoration",
     "Caransebes",
     "Romania",
     "detailing interior",
@@ -42,7 +37,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ro">
-      <link rel="icon" type="image/x-icon" href="assets/logo.jpg"></link>
+      <link rel="icon" type="image/x-icon" href="assets/logo.jpg" />
+      <link rel="canonical" href="https://www.luxurydetailing.ro/" />
+      <meta property="og:title" content="Luxury Detailing Caransebeș" />
+      <meta property="og:url" content="https://www.luxurydetailing.ro/" />
+      <meta
+        property="og:description"
+        content="Luxury Detailing este o companie stabilita in Caransebes ce oferă servicii profesionale de detaliere auto pentru a restaura și menține frumusețea vehiculului dumneavoastră."
+      />
+      <meta property="og:image" content="./assets/logo.png" />
       <body>
         <Header />
         {children}
