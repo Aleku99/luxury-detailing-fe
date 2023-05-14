074eee76650d891ffc5aa749ef0c730c62972810
Alexandru Loghin <loghinalex19@gmail.com>
2023-05-14 13:46:07 +0300

    refactor: refactored gallery and services pages
    Signed-off-by: Alexandru Loghin <loghinalex19@gmail.com>

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -2,34 +2,21 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import noImageSvg from "../../../public/assets/no-photos.svg";
-import { ImageList, ImageListItem } from "@mui/material";
+import { ImageList, ImageListItem, fabClasses } from "@mui/material";
 import { useCallback, useEffect, useState } from "react";
-import useSWR from "swr";
 import ImageViewer from "react-simple-image-viewer";
+import { galleryImageNames } from "@/lib/galleryImageNames";
 import { ColorRing } from "react-loader-spinner";
 
-const imagesFetcher = (url: any) =>
-  fetch(url, { cache: "no-store" })
-    .then((res) => res.json())
-    .then((images) =>
-      images.data.map(
-        (image: any) =>
-          `${process.env.NEXT_PUBLIC_DOMAIN}${image.attributes.image.data.attributes.url}`
-      )
-    )
-    .catch((error) => {
-      throw new Error(error);
-    });
-
 const Gallery = () => {
-  const { data, error, isLoading } = useSWR(
-    `${process.env.NEXT_PUBLIC_DOMAIN}/api/images?populate=*`,
-    imagesFetcher
-  );
-  const images = data;
+  const [isLoading, setIsLoading] = useState(true);
   const [currentImage, setCurrentImage] = useState(0);
   const [isViewerOpen, setIsViewerOpen] = useState(false);
   const [galleryCols, setGalleryCols] = useState(1);
+
+  const images = galleryImageNames.map(
+    (imageName) => `./assets/gallery/${imageName}`
+  );
 
   const openImageViewer = useCallback((index: number) => {
     setCurrentImage(index);
@@ -48,6 +35,7 @@ const Gallery = () => {
   useEffect(() => {
     handleResize();
     window.addEventListener("resize", handleResize);
+    setIsLoading(false);
 
     return () => {
       window.removeEventListener("resize", handleResize);
@@ -68,52 +56,54 @@ const Gallery = () => {
         />
       </div>
     );
-  } else if (error || images.length === 0) {
-    return (
-      <div className="min-h-[100vh] p-8 bg-black flex flex-col justify-center items-center">
-        <h2 className="text-2xl text-white text-shadow text-center ">
-          Ooops! Momentan nu avem nimic de afisat aici.
-        </h2>
-
-        <div className="relative w-[100px] h-[100px] md:w-[200px] md:h-[200px] mt-32 mb-48">
-          <Image
-            className="invert"
-            src={noImageSvg}
-            alt="Nu sunt imagini de afisat"
-            fill
-          />
-        </div>
-      </div>
-    );
   } else {
-    return (
-      <div className="min-h-[100vh] bg-black flex flex-col justify-start items-center">
-        <ImageList gap={8} cols={galleryCols} variant="standard">
-          {images.map((image: any, index: number) => (
-            <ImageListItem key={index}>
-              <img
-                src={`${image}`}
-                srcSet={`${image}`}
-                alt={`Gallery image ${index}`}
-                loading="lazy"
-                onClick={() => openImageViewer(index)}
-                style={{ cursor: "pointer" }}
-              />
-            </ImageListItem>
-          ))}
-        </ImageList>
-        {isViewerOpen && (
-          <ImageViewer
-            disableScroll
-            src={images}
-            currentIndex={currentImage}
-            closeOnClickOutside={true}
-            onClose={closeImageViewer}
-            backgroundStyle={{ zIndex: 999 }}
-          />
-        )}
-      </div>
-    );
+    if (!images || (images as string[]).length === 0) {
+      return (
+        <div className="min-h-[100vh] p-8 bg-black flex flex-col justify-center items-center">
+          <h2 className="text-2xl text-white text-shadow text-center ">
+            Ooops! Momentan nu avem imagini de afisat.
+          </h2>
+
+          <div className="relative w-[100px] h-[100px] md:w-[200px] md:h-[200px] mt-32 mb-48">
+            <Image
+              className="invert"
+              src={noImageSvg}
+              alt="Nu sunt imagini de afisat"
+              fill
+            />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="min-h-[100vh] bg-black flex flex-col justify-start items-center">
+          <ImageList gap={8} cols={galleryCols} variant="standard">
+            {(images as string[]).map((image: string, index: number) => (
+              <ImageListItem key={index}>
+                <img
+                  src={`${image}`}
+                  srcSet={`${image}`}
+                  loading="eager"
+                  alt={`Gallery image ${index}`}
+                  onClick={() => openImageViewer(index)}
+                  style={{ cursor: "pointer" }}
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+          {isViewerOpen && (
+            <ImageViewer
+              disableScroll
+              src={images}
+              currentIndex={currentImage}
+              closeOnClickOutside={true}
+              onClose={closeImageViewer}
+              backgroundStyle={{ zIndex: 999 }}
+            />
+          )}
+        </div>
+      );
+    }
   }
 };
 
---
+++ src/lib/services.ts
@@ -0,0 +1,6 @@
+export interface Service {
+  name: string;
+  price: number;
+}
+
+export const services: Service[] = [];
