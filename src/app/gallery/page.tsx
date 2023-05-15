6a2a0a238623835bc0404beeadcb2f8e196445fe
Alexandru Loghin <58110813+Aleku99@users.noreply.github.com>
2023-05-16 01:05:48 +0300

    fix initial gallery page (#25)
    * fix: set initial gallery page to 0
    
    Signed-off-by: Alexandru Loghin <loghinalex19@gmail.com>
    
    * fix: let image viewer access all images regarding what is displayed on current page
    
    Signed-off-by: Alexandru Loghin <loghinalex19@gmail.com>
    
    ---------
    
    Signed-off-by: Alexandru Loghin <loghinalex19@gmail.com>

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -12,7 +12,7 @@ const Gallery = () => {
   const [currentImage, setCurrentImage] = useState(0);
   const [isViewerOpen, setIsViewerOpen] = useState(false);
   const [galleryCols, setGalleryCols] = useState(1);
-  const [page, setPage] = useState(4);
+  const [page, setPage] = useState(0);
 
   const galleryImagesSrcs = galleryImageNames.map(
     (name) => `./assets/gallery/${name}`
@@ -131,10 +131,7 @@ const Gallery = () => {
           {isViewerOpen && (
             <ImageViewer
               disableScroll
-              src={galleryImagesSrcs.slice(
-                page * imagesPerPage,
-                (page + 1) * imagesPerPage
-              )}
+              src={galleryImagesSrcs}
               currentIndex={currentImage}
               closeOnClickOutside={true}
               onClose={closeImageViewer}
